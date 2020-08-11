4597f3700a07096f7d70267eaf1bea54173e55c1
jjswift <supercheater2018@yandex.ru>
2020-08-11 22:41:26 +0300

    thunks

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
 import { useEffect } from 'react'
 import { getTodos } from '../redux/todo-reducer'
 
 export const Todo = (props) => {
 
     useEffect(() => {
-        getTodos()
-        console.log('Inside TODO.jsx')
+        
+        getTodos('1')
+        
+
     })
 
     return (
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,21 +1,32 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware, createStore } from '@reduxjs/toolkit';
 import { todoReducer } from './todo-reducer';
-/*
+//import { createStore, combineReducers, applyMiddleware } from "redux";
+import ReduxThunk from 'redux-thunk';
+////export const store = configureStore({
+////    reducer: {
+////        todoReducer
+////    },
+////    middleware: getDefaultMiddleware(),
+////    devTools: process.env.NODE_ENV !== 'production',
+////});
+//
+//let reducers = combineReducers({
+//    todoReducer
+//})
+//
+//export const store = createStore(reducers, applyMiddleware(ReduxThunk));
 
-const middleware = getDefaultMiddleware({
-    immutableCheck: false,
-    serializableCheck: false,
-    thunk: true,
-}); 
-
-*/
+const middleware = [...getDefaultMiddleware(), ReduxThunk]
 
 export const store = configureStore({
     reducer: {
         todoReducer
     },
-    middleware: getDefaultMiddleware(),
+    middleware,
     devTools: process.env.NODE_ENV !== 'production',
-});
+})
+
+
 
 window.store = store
+
