5c97fa62aa7b123bd455a3da412800594c6c5567
jjswift <supercheater2018@yandex.ru>
2020-08-12 16:41:13 +0300

    Finally good hooks

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { getTodos } from '../redux/todo-reducer'
+import { useSelector, useCallback, useDispatch } from 'react-redux'
 
 export const Todo = (props) => {
 
-    useEffect(() => {
-        
-        getTodos('1')
-        
+    const state = useSelector(state => state)
+    const dispatch = useDispatch()
+    
+    useEffect( () => {   
+        dispatch(getTodos())
+    }, [dispatch]) 
 
+    useEffect( () => {
+        console.log(state)
     })
+    
+    
 
     return (
         <div>
-            Todo page
+            {state.todoReducer.taskCount + state.todoReducer.tasks}
         </div>
     )
 }
