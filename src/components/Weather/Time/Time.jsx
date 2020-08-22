27f2ff59612a166a7b48b47d93caf366c6cad34b
jjswift <supercheater2018@yandex.ru>
2020-08-23 01:11:45 +0300

    Time deletions

--- src/components/Weather/Time/Time.jsx
+++ src/components/Weather/Time/Time.jsx
@@ -1,24 +1,5 @@
 import React, { useState, useEffect } from 'react'
 
-//export const Time = (props) => {
-//
-//    const [time, setTime] = useState(new Date().toLocaleTimeString())
-//    const tick = setTime(new Date().toLocaleTimeString())
-//        
-//    console.log('Time component render')
-//
-//    useEffect(() => {
-//        const timer = setInterval(() => tick(), 1000);
-//       //a return clearInterval(timer)
-//    }, [tick])
-//    
-//    return (
-//        <div>
-//            {time}
-//        </div>
-//    )
-//}
-
 export const Time = props => {
     const [time, setTime] = useState(new Date())
     const tick = () => { setTime(new Date()) }
@@ -30,7 +11,7 @@ export const Time = props => {
         )
         return () => {clearInterval(timerID)}
     }, [])
-    
+
     return(
         <div>
             Time now: {time.toLocaleTimeString()}
