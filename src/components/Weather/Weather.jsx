d4493028a581d105447d71bbd865ff1e072a1a32
jjswift <supercheater2018@yandex.ru>
2020-08-23 00:33:21 +0300

    WeatherInfo component

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,15 +3,19 @@ import React, { useCallback, useEffect , useMemo } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { Time } from './Time/Time'
 import { getWeatherForCurrentPos, geocodeCurrentUserPosition, getUserPosition, togglePositionReceivedStatus } from './../redux/weather-reducer';
+import { WeatherInfo } from './WeatherInfo/WeatherInfo';
 
 // todo: time must to be component!
 // todo: dispatch geolocation. (withouth thunks) DONE, BUT WITH THUNK :D;
 export const Weather = (props) => {
     const state = useSelector(state => state.weatherReducer)
     const dispatch = useDispatch()
-    console.log('render compoentn')
+    console.log('render component')
     useEffect(() => { // Получаю геолокацию пользователя
+        
+
         if (state.isPositionReceived) return
+        console.log('Юзэффект с получением пользовательской геолокации')
         dispatch(getUserPosition()) 
         dispatch(togglePositionReceivedStatus())
     }, [])
@@ -21,20 +25,21 @@ export const Weather = (props) => {
         unit_system: 'si'
     }
     useEffect(() => {
+        console.log('Юзэффект с получением погоды и геокодингом позиции юзера')
         if (!state.location.city) dispatch(geocodeCurrentUserPosition({ ...state.position }))
         dispatch(getWeatherForCurrentPos({ ...state.position, ...weatherParams  }))
     }, [state.position])
 
     return (
         <div>
-            <h3>Weather<br/></h3>
+            <h1>Weather</h1>
             <Time />
             <p>
                 Your location is: {!state.location.country ? 'Loading...' : state.location.country + ', ' + state.location.city}
             </p>
             <div>
-                Lifetime weather for your position: 
-                {state.isWeatherFetching ? 'Loading..' : !state.weather.temp ? '' : state.weather.temp.value + state.weather.temp.units}
+                
+                {state.isWeatherFetching ? 'Loading.......' : <WeatherInfo weather={state.weather}/>}
             </div>
         </div>
     )
--- src/components/Weather/WeatherInfo/WeatherInfo.jsx
+++ src/components/Weather/WeatherInfo/WeatherInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const WeatherInfo = (props) => {
     return (
         <div>
-            WeatherInfo
+            Lifetime weather for your position: {!props.weather.temp ? '' : props.weather.temp.value + props.weather.temp.units}
         </div>
     )
 }
